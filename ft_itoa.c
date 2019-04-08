d481341009039125f935c77aa41230ea458856a6
Unknown <leonard.achille@gmail.com>
2019-04-08 09:52:25 +0200

    v0.7
    itoa check + 42norme

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,16 +6,16 @@
 /*   By: lachille <lachille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 04:32:56 by lachille          #+#    #+#             */
-/*   Updated: 2019/04/08 09:30:23 by lachille         ###   ########.fr       */
+/*   Updated: 2019/04/08 09:51:04 by lachille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*alloc_ar(int n)
 {
 	char	*dst;
-  int i;
+	int		i;
 
-  i = 0;
+	i = 0;
 	if (n < 0)
 		i += 1;
 	while (n > 9)
@@ -23,28 +23,29 @@ char	*alloc_ar(int n)
 		n = n / 10;
 		i += 1;
 	}
-	if(!(dst = malloc(sizeof(*dst) * i + 50)))
+	if (!(dst = malloc(sizeof(*dst) * i + 50)))
 		return (0);
-  dst[i + 1] = '\0';
-	return(dst);
+	dst[i + 1] = '\0';
+	return (dst);
 }
 
-int ft_len(int n, int i)
+int		ft_len(int n, int i)
 {
-  i = 0;
+	i = 0;
 	if (n < 0)
-  {
+	{
 		i += 1;
-    n *= -1;
-  }
+		n *= -1;
+	}
 	while (n > 9)
 	{
 		n = n / 10;
 		i += 1;
 	}
-  return (i);
+	return (i);
 }
-char	 *recursive(int i, int n, char *dst, int len)
+
+char	*recursive(int i, int n, char *dst, int len)
 {
 	int		nb;
 
@@ -53,27 +54,27 @@ char	 *recursive(int i, int n, char *dst, int len)
 		dst = alloc_ar(n);
 		dst[0] = '-';
 		n *= -1;
-    len = -42;
+		len = -42;
 	}
 	if (i == len)
 		dst = alloc_ar(n);
 	if (n < 10)
 	{
 		dst[i] = n + 48;
-		return(dst);
+		return (dst);
 	}
-  nb = n % 10 + 48;
+	nb = n % 10 + 48;
 	dst[i] = nb;
 	return (recursive(i - 1, n / 10, dst, len));
 }
 
 char	*ft_itoa(int n)
 {
-	int	i;
-  char *dst;
-  int len;
+	int		i;
+	char	*dst;
+	int		len;
 
-  len = ft_len(n, i);
-  i = len;
-	return(recursive(i, n, dst, len));
+	len = ft_len(n, i);
+	i = len;
+	return (recursive(i, n, dst, len));
 }

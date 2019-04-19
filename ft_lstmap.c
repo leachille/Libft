9daec3c54272d42abfb2ca70dc56a483dcb59711
Unknown <leonard.achille@gmail.com>
2019-04-19 06:21:49 +0200

    v1.3

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,32 +6,32 @@
 /*   By: lachille <lachille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 16:01:05 by lachille          #+#    #+#             */
-/*   Updated: 2019/04/18 16:36:58 by lachille         ###   ########.fr       */
+/*   Updated: 2019/04/19 06:16:27 by lachille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list * ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list *first;
+	t_list *element;
 	t_list *list2;
-	if (!lst || !*f)
+
+	if (!lst)
 		return (NULL);
-	list2 = NULL;
+	first = NULL;
 	while (lst)
 	{
 		list2 = (*f)(lst);
-		if (first)
-		{
-			first->next = list2;
-			first = first->next;
-		}
-		else
+		if (!(first))
 		{
 			first = list2;
+			element = first;
 		}
+		element->next = list2;
+		element = element->next;
 		lst = lst->next;
 	}
-	return (list2);
+	return (first);
 }

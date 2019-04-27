d08b8defd9a8d1f5df5914e77526912e65e456f5
Unknown <leonard.achille@gmail.com>
2019-04-28 01:21:52 +0200

    3.3.1

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: lachille <lachille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 22:48:38 by lachille          #+#    #+#             */
-/*   Updated: 2019/04/28 00:25:25 by lachille         ###   ########.fr       */
+/*   Updated: 2019/04/28 01:19:42 by lachille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,16 @@ char	*ft_realloc(char *s1, char *add)
 	j = 0;
 	if (!(tmp = malloc(sizeof(s1) * (ft_strlen(s1) + ft_strlen(add) + 1))))
 		return (0);
-while (add[j])
-{
-	while (s1[i])
+	while (add[j])
 	{
-		tmp[i] = s1[i];
-		i++;
+		while (s1[i])
+		{
+			tmp[i] = s1[i];
+			i++;
+		}
+		tmp[i + j] = add[j];
+		j++;
 	}
-	tmp[i + j] = add[j];
-	j++;
-}
-tmp[i + j] = '\0';
+	tmp[i + j] = '\0';
 	return (tmp);
 }
